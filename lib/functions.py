7e5cb5ca69d6ce2741509c482de0e41543591c94
Paul Aiamsubhab <paul.aiamsubhab@gmail.com>
2024-01-24 01:50:23 -0500

    complete lab

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,31 @@
 #!/usr/bin/env python3
-
+# /*
+#   You should be able to call this function with no arguments and see its output in the terminal:
+#   greetProgrammer();
+#   => "Hello, programmer!"
+# */
+# function greetProgrammer() {
+#   console.log("Hello, programmer!");
+# }
 def greet_programmer():
-    pass
+    print('Hello, programmer!')
 
+# /*
+#   You should be able to call this function with one argument and see its output in the terminal:
+#   greet("Naureen");
+#   => "Hello, Naureen!"
+# */
+# function greet(name) {
+#   console.log(`Hello, ${name}!`);
+# }
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
 
 def add(num1, num2):
-    pass
+    return num1 + num2
 
 def halve(number):
-    pass
+    return number / 2
